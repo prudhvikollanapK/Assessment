3b6c143760cdf1b88e8b7fa921015251811b679c
pK <86195686+prudhvikollanapK@users.noreply.github.com>
2024-05-10 11:02:40 +0530

    added admin interface

--- DataPusher/admin.py
+++ DataPusher/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class AccountModel(admin.ModelAdmin):
+    list_display=("email","account_id","account_name","app_secret_token","website")
+
+admin.site.register(Account, AccountModel)
+
+class DestinationModel(admin.ModelAdmin):
+    list_display=("account","url","http_method","headers")
+
+admin.site.register(Destination, DestinationModel)
